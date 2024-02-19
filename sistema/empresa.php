c9a3c405b53cbad569a92af1b933f0cd929df518
Jose Luis Nievas <joseluisnievas123456@gmail.com>
2024-02-19 00:56:24 -0300

    cambios en empresa e index de la carpeta sistema

--- sistema/empresa.php
+++ sistema/empresa.php
@@ -8,7 +8,7 @@ $txtTelefono = $_POST['txtTelEmpresa'];
 $txtDireccion = $_POST['txtDirEmpresa'];
 $txtemail = $_POST['txtEmailEmpresa'];
 $txtigv = $_POST['txtIgv'];
-$actualizar_empresa = mysqli_query($conexion, "UPDATE configuracion SET dni = $txtdni, nombre = '$txtNombre', razon_social = '$txtRSocial', telefono = $txtTelefono, email = '$txtemail', direccion = '$txtDireccion', igv = $txtigv");
+$actualizar_empresa = mysqli_query($conexion, "UPDATE configuracion SET dni = '$txtdni', nombre = '$txtNombre', razon_social = '$txtRSocial', telefono = '$txtTelefono', email = '$txtemail', direccion = '$txtDireccion', igv = $txtigv");
 mysqli_close($conexion);
 if ($actualizar_empresa) {
   $alert = '<p class="msg_save">Configuración de empresa Actualizado</p>';
--- sistema/index.php
+++ sistema/index.php
@@ -145,8 +145,8 @@
 					<div class="card-body">
 						<form action="empresa.php" method="post" id="frmEmpresa" class="p-3">
 							<div class="form-group">
-								<label>Ruc:</label>
-								<input type="number" name="txtDni" value="<?php echo $dni; ?>" id="txtDni" placeholder="Dni de la Empresa" required class="form-control">
+								<label>CUIT:</label>
+								<input type="text" name="txtDni" value="<?php echo $dni; ?>" id="txtDni" placeholder="Dni de la Empresa" required class="form-control">
 							</div>
 							<div class="form-group">
 								<label>Nombre:</label>
@@ -158,7 +158,7 @@
 							</div>
 							<div class="form-group">
 								<label>Teléfono:</label>
-								<input type="number" name="txtTelEmpresa" class="form-control" value="<?php echo $telEmpresa; ?>" id="txtTelEmpresa" placeholder="teléfono de la Empresa" required>
+								<input type="text" name="txtTelEmpresa" class="form-control" value="<?php echo $telEmpresa; ?>" id="txtTelEmpresa" placeholder="teléfono de la Empresa" required>
 							</div>
 							<div class="form-group">
 								<label>Correo Electrónico:</label>
@@ -169,7 +169,7 @@
 								<input type="text" name="txtDirEmpresa" class="form-control" value="<?php echo $dirEmpresa; ?>" id="txtDirEmpresa" placeholder="Dirreción de la Empresa" required>
 							</div>
 							<div class="form-group">
-								<label>IGV (%):</label>
+								<label>IVA (%):</label>
 								<input type="text" name="txtIgv" class="form-control" value="<?php echo $igv; ?>" id="txtIgv" placeholder="IGV de la Empresa" required>
 							</div>
 							<?php echo isset($alert) ? $alert : ''; ?>
@@ -190,7 +190,7 @@
 					<div class="card-body">
 						<div class="p-3">
 							<div class="form-group">
-								<strong>Ruc:</strong>
+								<strong>CUIT:</strong>
 								<h6><?php echo $dni; ?></h6>
 							</div>
 							<div class="form-group">
@@ -214,7 +214,7 @@
 								<h6><?php echo $dirEmpresa; ?></h6>
 							</div>
 							<div class="form-group">
-								<strong>IGV (%):</strong>
+								<strong>IVA (%):</strong>
 								<h6><?php echo $igv; ?></h6>
 							</div>
 
